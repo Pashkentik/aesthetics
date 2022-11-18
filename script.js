b295a8191db96227524e155bdd1e2f34f2cc3d16
pashkentik <pavlo.prustypa@gmail.com>
2022-11-18 22:25:04 +0200

    щдщд

--- script.js
+++ script.js
@@ -25,3 +25,32 @@ function backToTop() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+class ItcAccordion {
+  constructor(target, config) {
+    this._el = typeof target === 'string' ? document.querySelector(target) : target;
+    const defaultConfig = {
+      alwaysOpen: true
+    };
+    this._config = Object.assign(defaultConfig, config);
+    this.addEventListener();
+  }
+
+  addEventListener() {
+    this._el.addEventListener('click', (e) => {
+      const elHeader = e.target.closest('.faq_list_item_group_title');
+      if (!elHeader) {
+        return;
+      }
+      if (!this._config.alwaysOpen) {
+        const elOpenItem = this._el.querySelector('.faq_show');
+        if (elOpenItem) {
+          elOpenItem !== elHeader.parentElement ? elOpenItem.classList.toggle('faq_show') : null;
+        }
+      }
+      elHeader.parentElement.classList.toggle('faq_show');
+    });
+  }
+}
+
+new ItcAccordion('#accordion_1');
